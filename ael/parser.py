c114345140952f920ed6a8ddbdc5746201e0e0b2
Ray Toal <rtoal@lmu.edu>
2020-12-29 22:33:10 -0800

    Refactor scanner and parser

--- ael/parser.py
+++ ael/parser.py
@@ -1,22 +1,55 @@
 """Parser
 
-This is a recursive descent parser. Each category ("non-termnial") gets its
-own parsing function which returns a piece of the Abstract Syntax Tree or
-raises an Exception. More information about Recursive Descent Parsing can
-be found at https://en.wikipedia.org/wiki/Recursive_descent_parser.
+This is a recursive descent parser. Each phrase rule ("non-termnial") gets
+its own parsing function which returns a piece of the Abstract Syntax Tree or
+raises an Exception. More information about Recursive Descent Parsing can be
+found at https://en.wikipedia.org/wiki/Recursive_descent_parser.
 
-The parser is implemented as a single function which creates a scanner when it
-begins.
+The parser is implemented as a single function which creates a scanner when
+it begins. The parser stores the current token in the two local variables
+(category, lexeme), but rather than using the variables directly it relies
+on two utility functions:
+
+    match(t)
+        Expect the next token to be t or in the set t, consume it, then return
+        its lexeme. If the next token is not t or in the set t, raise an Error.
+
+    match()
+        Consume and return the next token, whatever it is.
+
+    at(t)
+        Whether or not the next token is t or in the set t.
+
+When calling match() or at(), you can supply either a category or a lexeme:
+
+    match("#IDENTIFIER")
+    match("=")
+    match({"+", "-", "#NUMBER"})
+    at({"/", "*"})
 """
 
-from ael.scanner import Scanner
+from ael.scanner import tokenize
 from ael.ast import *
 
 
 def parse(source_code):
-    the_scanner = Scanner(source_code)
-    at = the_scanner.at
-    match = the_scanner.match
+    token_stream = tokenize(source_code)
+    category, lexeme = next(token_stream)
+
+    def at(candidate):
+        if isinstance(candidate, set):
+            return any(at(c) for c in candidate)
+        if candidate.startswith('#'):
+            return category == candidate
+        return lexeme == candidate
+
+    def match(expected_token=None):
+        nonlocal category, lexeme
+        if expected_token is None or at(expected_token):
+            matched_lexeme = lexeme
+            category, lexeme = next(token_stream)
+            return matched_lexeme
+        raise Exception(f"Expected '{expected_token}'")
 
     def parse_program():
         statements = []
--- ael/scanner.py
+++ ael/scanner.py
@@ -1,36 +1,17 @@
 """Scanner
 
-This is a hand-crafted scanner, using no external libraries at all. To scan,
-create an instance of the Scanner class with the source code as a string, and
-invoke, as-needed:
+This is a hand-crafted scanner, using no external libraries at all. The
+module exports a single generator function, tokenize, to which you pass the
+source code of the program to tokenize.
 
-    match(t)
-        Expect the next token to be t or in the set t, consume it, then return
-        its lexeme. If the next token is not t or in the set t, raise an Error.
-
-    match()
-        Consume and return the next token, whatever it is.
-
-    at(t)
-        Whether or not the next token is t or in the set t.
-
-Tokens are represented internally as tuples of the form (category, lexeme).
-Categories always begin with a # character. Examples:
+Tokens are yielded as tuples of the form (category, lexeme). Categories
+always begin with a # character. There are four types of categories, fully
+listed in the following examples:
 
     ("#IDENTFIER", "x")
-    ("#NUMBER", 32767)
+    ("#NUMBER", "153.8831")
     ("#SYMBOL", "+")
     ("#KEYWORD", "let")
-
-When calling match() or at(), you can supply either a category or a lexeme:
-
-    match("#IDENTIFIER")
-    match("=")
-    match({"+", "-", "#NUMBER"})
-    at({"/", "*"})
-
-This module also exports the tokenize function that the scanner uses to
-generate the token sequence, good for inspecting the token stream.
 """
 
 import re
@@ -64,26 +45,3 @@ def tokenize(source):
         yield (category, match.group(0).strip())
         position = match.end()
     yield ('#END', '')
-
-
-class Scanner:
-    def __init__(self, source_code):
-        self.stream = tokenize(source_code)
-        self.advance()
-
-    def advance(self):
-        self.category, self.lexeme = next(self.stream)
-
-    def at(self, candidate):
-        if isinstance(candidate, set):
-            return any(self.at(c) for c in candidate)
-        if candidate.startswith('#'):
-            return self.category == candidate
-        return self.lexeme == candidate
-
-    def match(self, expected_token=None):
-        if expected_token is None or self.at(expected_token):
-            matched_lexeme = self.lexeme
-            self.advance()
-            return matched_lexeme
-        raise Exception(f"Expected '{expected_token}'")
